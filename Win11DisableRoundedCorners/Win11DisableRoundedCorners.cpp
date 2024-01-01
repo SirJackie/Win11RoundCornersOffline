9b83de27f7d1c86c25c2cf3e69476697283fc273
lyj00912 <3122742760@qq.com>
2024-01-01 22:54:23 +0800

    tried to fully cpp-fy the program, but temporarily stuck in UNICODE type converting problem, and decided to use another way to do this.

--- Win11DisableRoundedCorners/Win11DisableRoundedCorners.cpp
+++ Win11DisableRoundedCorners/Win11DisableRoundedCorners.cpp
@@ -1,6 +1,7 @@
-#undef UNICODE
 #include <Windows.h>
 #include <conio.h>
+#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
 
 extern "C" {
 #include <valinet/pdb/pdb.h>
@@ -8,186 +9,138 @@ extern "C" {
 #include <valinet/utility/takeown.h>
 }
 
+std::string AskSysDir() {
+	char sysDir[MAX_PATH];
+	ZeroMemory(
+		sysDir,
+		(MAX_PATH) * sizeof(char)
+	);
+	GetSystemDirectoryA(
+		sysDir,
+		MAX_PATH
+	);  // sysDir = C:\Windows\System32
+	return std::string(sysDir);
+}
+
+std::string AskCWD() {
+	char cwd[_MAX_PATH];
+	ZeroMemory(
+		cwd,
+		(_MAX_PATH) * sizeof(char)
+	);
+	GetModuleFileNameA(
+		GetModuleHandle(NULL),
+		cwd,
+		_MAX_PATH
+	);
+	PathRemoveFileSpecA(cwd);
+	// modFn => C:\Users\Windows To Go\Desktop\Win11DisableRoundedCorners\x64\Debug
+	return std::string(cwd);
+}
+
+int FileExists(std::string file) {
+	return fileExists((char*)file.c_str());
+}
+
+void KillDWM() {
+	std::string szTaskkill = "\"" + AskSysDir() + "\\taskkill.exe\" /f /im dwm.exe";  // "C:\Windows\system32\taskkill.exe\" /f /im dwm.exe
+
+	STARTUPINFO si = { sizeof(si) };
+	PROCESS_INFORMATION pi;
+	BOOL b = CreateProcessA(
+		NULL,
+		(LPSTR)szTaskkill.c_str(),
+		NULL,
+		NULL,
+		TRUE,
+		CREATE_UNICODE_ENVIRONMENT,
+		NULL,
+		NULL,
+		&si,
+		&pi
+	);
+	WaitForSingleObject(pi.hProcess, INFINITE);
+	Sleep(10000);
+}
+
+std::string DownloadSymbol(std::string dllFilename) {
+	// Input: CWD\uDWM.dll
+	// Effect: Download and Create CWD\uDWM.pdb
+	// Output: CWD\uDWM.pdb
+
+	char dllFn[_MAX_PATH];
+	ZeroMemory(
+		dllFn,
+		(_MAX_PATH) * sizeof(char)
+	);
+	strcpy(dllFn, dllFilename.c_str());  // dllFn = CWD\uDWM.dll
+
+	// Download: CWD\uDWM.pdb
+	// Side Effect: Setting dllFn to CWD\uDWM.pdb
+	if (VnDownloadSymbols(
+		NULL,
+		dllFn,
+		dllFn,
+		_MAX_PATH
+	))
+	{
+		printf(
+			"Unable to download symbols. Make sure you have a working Internet "
+			"connection.\n"
+		);
+		_getch();
+	}
+	return std::string(dllFn);  // Output: CWD\uDWM.pdb
+}
+
 int main(int argc, char** argv)
 {
+
     BOOL bRestore = FALSE;
 
-	/*
-	** Save szTaskkill（杀死进程的命令）
-	*/
-	// szTaskkill = C:\Windows\system32\taskkill.exe /f /im dwm.exe
-    char szTaskkill[MAX_PATH];
-    ZeroMemory(
-        szTaskkill,
-        (MAX_PATH) * sizeof(char)
-    );
-    szTaskkill[0] = '\"';
-    GetSystemDirectoryA(
-        szTaskkill + sizeof(char),
-        MAX_PATH
-    );  // szTaskkill => C:\Windows\System32
-    strcat_s(
-        szTaskkill,
-        MAX_PATH,
-        "\\taskkill.exe\" /f /im dwm.exe"
-    );  // szTaskkill => C:\Windows\system32\taskkill.exe /f /im dwm.exe
-
-	/*
-	** Save szPath（当前EXE名字）
-	*/
-	// szPath = Win11DisableOrRestoreRoundedCorners.exe
-    char szPath[_MAX_PATH];
-    ZeroMemory(
-        szPath,
-        (_MAX_PATH) * sizeof(char)
-    );
-    GetModuleFileNameA(
-        GetModuleHandle(NULL),
-        szPath,
-        _MAX_PATH
-    );  // szPath => C:\Users\Windows To Go\Desktop\Win11DisableRoundedCorners\x64\Debug\Win11DisableOrRestoreRoundedCorners.exe
-    PathStripPathA(szPath);  // szPath => Win11DisableOrRestoreRoundedCorners.exe
-
-	/*
-	** Save szOriginalPath（未修改的原始DLL备份）
-	*/
-	// szOriginalDWM = C:\Windows\system32\uDWM_win11drc.bak
-    char szOriginalDWM[_MAX_PATH];
-    ZeroMemory(
-        szOriginalDWM,
-        (_MAX_PATH) * sizeof(char)
-    );
-    GetSystemDirectoryA(
-        szOriginalDWM,
-        MAX_PATH
-    );
-    strcat_s(
-        szOriginalDWM,
-        MAX_PATH,
-        "\\uDWM_win11drc.bak"
-    );
+	std::string szOriginalDWM = AskSysDir() + "\\uDWM_win11drc.bak";  // System32\uDWM_win11drc.bak
+	std::string szDWM = AskSysDir() + "\\uDWM.dll";  // System32\uDWM.dll
 
 	/*
 	** Determine Patch/Restore
 	*/
-    bRestore = fileExists(szOriginalDWM);  // Patch: 0, Restore: 1
+    bRestore = FileExists(szOriginalDWM);  // Patch: 0, Restore: 1
+
+	// Get String: szModifiedDWM
+	std::string szModifiedDWM;
+	// RESTORE MODE: System32\uDWMm.dll
+    if (bRestore) szModifiedDWM = AskSysDir() + "\\uDWMm.dll";  // 
+	// PATCH MODE: CWD\uDWM.dll
+	else szModifiedDWM = AskCWD() + "\\uDWM.dll";
 
 	/*
-	** Save szModifiedPath（已修改的DLL，PATCH模式指向下载的DLL，RESTORE模式指向System32的DLL）
-	*/
-	// PATCH MODE: szModifiedDWM = C:\Users\Windows To Go\Desktop\Win11DisableRoundedCorners\x64\Debug\uDWM.dll
-	// RESTORE MODE: szModifiedDWM = C:\Windows\system32\uDWMm.dll
-    char szModifiedDWM[_MAX_PATH];
-    ZeroMemory(
-        szModifiedDWM,
-        (_MAX_PATH) * sizeof(char)
-    );
-    if (bRestore)  // RESTORE MODE
-    {
-        /*
-        char szSfcscannow[MAX_PATH];
-        ZeroMemory(
-            szSfcscannow,
-            (MAX_PATH) * sizeof(char)
-        );
-        szSfcscannow[0] = '\"';
-        GetSystemDirectoryA(
-            szSfcscannow + sizeof(char),
-            MAX_PATH
-        );
-        strcat_s(
-            szSfcscannow,
-            MAX_PATH,
-            "\\sfc.exe\" /scannow"
-        );
-        STARTUPINFO si = {sizeof(si)};
-        PROCESS_INFORMATION pi;
-        BOOL b = CreateProcessA(
-            NULL,
-            szSfcscannow,
-            NULL,
-            NULL,
-            TRUE,
-            CREATE_UNICODE_ENVIRONMENT,
-            NULL,
-            NULL,
-            &si,
-            &pi
-        );
-        WaitForSingleObject(pi.hProcess, INFINITE);
-        DeleteFileA(szOriginalDWM);
-        printf("Operation successful.\n");
-        Sleep(10000);
-        ExitWindowsEx(EWX_REBOOT | EWX_FORCE | EWX_FORCEIFHUNG, SHTDN_REASON_FLAG_PLANNED);
-        exit(0);
-        */
-        GetSystemDirectoryA(
-            szModifiedDWM,
-            MAX_PATH
-        );    
-        strcat_s(
-            szModifiedDWM,
-            MAX_PATH,
-            "\\uDWMm.dll"
-        );  // RESTORE MODE: szModifiedDWM = C:\Windows\system32\uDWMm.dll
-    }
-    else  // PATCH MODE
-    {
-        GetModuleFileNameA(
-            GetModuleHandle(NULL),
-            szModifiedDWM,
-            _MAX_PATH
-        );
-        PathRemoveFileSpecA(szModifiedDWM);
-        strcat_s(
-            szModifiedDWM,
-            MAX_PATH,
-            "\\uDWM.dll"
-        );  // PATCH MODE: szModifiedDWM = C:\Users\Windows To Go\Desktop\Win11DisableRoundedCorners\x64\Debug\uDWM.dll
-    }
-
-	/*
-	** Save szDWM（系统的DLL）
-	*/
-	// szDWM = C:\Windows\system32\uDWM.dll
-    char szDWM[MAX_PATH];
-    ZeroMemory(
-        szDWM,
-        (MAX_PATH) * sizeof(char)
-    );
-    GetSystemDirectoryA(
-        szDWM,
-        MAX_PATH
-    );
-    strcat_s(
-        szDWM,
-        MAX_PATH,
-        "\\uDWM.dll"
-    );  // szDWM = C:\Windows\system32\uDWM.dll
-
-	/*
-	** RESTORE模式
+	** RESTORE MODE
 	*/
     if (bRestore)
     {
-        DeleteFileA(szModifiedDWM);  // 删除: "C:\\Windows\\system32\\uDWMm.dll"
-        if (!MoveFileA(szDWM, szModifiedDWM))  // 移动: "C:\\Windows\\system32\\uDWM.dll" => "C:\\Windows\\system32\\uDWMm.dll"
+        DeleteFileA((LPCSTR)szModifiedDWM.c_str());  // Delete System32\uDWMm.dll
+        if (!MoveFileA((LPCSTR)szDWM.c_str(), (LPCSTR)szModifiedDWM.c_str()))  // Rename: System32\uDWM.dll => System32\uDWMm.dll
         {
             printf("Unable to restore DWM.\n");
             _getch();
             return 1;
         }
-        if (!MoveFileA(szOriginalDWM, szDWM))  // 移动: "C:\\Windows\\system32\\uDWM_win11drc.bak" => "C:\\Windows\\system32\\uDWM.dll"
+        if (!MoveFileA((LPCSTR)szOriginalDWM.c_str(), (LPCSTR)szDWM.c_str()))  // Rename: System32\uDWM_win11drc.bak => System32\uDWM.dll
         {
             printf("Unable to restore DWM.\n");
             _getch();
             return 2;
         }
+		KillDWM();  // Taskkill dwm.exe
+		DeleteFileA((LPCSTR)szModifiedDWM.c_str());  // Delete: System32\uDWMm.dll
     }
-	// Patch模式
+
+	/*
+	** PATCH MODE
+	*/
     else
     {
-        if (!CopyFileA(szDWM, szModifiedDWM, FALSE))  // 移动：C:\Windows\system32\uDWM.dll => C:\Users\Windows To Go\Desktop\Win11DisableRoundedCorners\x64\Debug\uDWM.dll
+        if (!CopyFileA((LPCSTR)szDWM.c_str(), (LPCSTR)szModifiedDWM.c_str(), FALSE))  // Copy：System32\uDWM.dll => CWD\uDWM.dll
         {
             printf(
                 "Temporary file copy failed. Make sure the application has write "
@@ -196,25 +149,18 @@ int main(int argc, char** argv)
             _getch();
             return 1;
         }
-        if (VnDownloadSymbols(
-            NULL,
-            szModifiedDWM,
-            szModifiedDWM,
-            _MAX_PATH
-        ))  // 下载：C:\Users\Windows To Go\Desktop\Win11DisableRoundedCorners\x64\Debug\uDWM.pdb
-			// Side Effect: Setting szModifiedDWM = C:\Users\Windows To Go\Desktop\Win11DisableRoundedCorners\x64\Debug\uDWM.pdb
-        {
-            printf(
-                "Unable to download symbols. Make sure you have a working Internet "
-                "connection.\n"
-            );
-            _getch();
-            return 2;
-        }
+        
+		std::string pdbFileName = DownloadSymbol(szModifiedDWM);
+
+		/*
+		** ----- Start Patching DLL using PDB File -----
+		*/
+
+		// Get Function Address in PDB File
         DWORD addr[1] = { 0 };
         char* name[1] = { "CTopLevelWindow::GetEffectiveCornerStyle" };
-        if (VnGetSymbols(  // Get Symbol Address in uDWM.pdb
-            szModifiedDWM,
+        if (VnGetSymbols(
+            (LPCSTR)pdbFileName.c_str(),
             addr,
             name,
             1
@@ -224,23 +170,21 @@ int main(int argc, char** argv)
             _getch();
             return 3;
         }
-        printf("Function address is: 0x%x.\n", addr[0]);
-        DeleteFileA(szModifiedDWM);  // Delete: szModifiedDWM = C:\User\\Windows To Go\Desktop\Win11DisableRoundedCorners\x64\Debug\uDWM.pdb
-        PathRemoveFileSpecA(szModifiedDWM);  // 设置: szModifiedDWM = C:\User\\Windows To Go\Desktop\Win11DisableRoundedCorners\x64\Debug
-        strcat_s(
-            szModifiedDWM,
-            MAX_PATH,
-            "\\uDWM.dll"
-        );  // 设置: szModifiedDWM = C:\User\\Windows To Go\Desktop\Win11DisableRoundedCorners\x64\Debug\uDWM.dll
+		printf("Function address is: 0x%x.\n", addr[0]);
+
+		// Delete: CWD\uDWM.pdb
+        DeleteFileA((LPCSTR)pdbFileName.c_str());
+
+        // Patch: CWD\uDWM.dll
         HANDLE hFile = CreateFileA(
-            szModifiedDWM,
+			(LPCSTR)szModifiedDWM.c_str(),
             GENERIC_READ | GENERIC_WRITE,
             FILE_SHARE_READ,
             NULL,
             OPEN_EXISTING,
             FILE_ATTRIBUTE_NORMAL,
             0
-        );  // 创建: C:\User\\Windows To Go\Desktop\Win11DisableRoundedCorners\x64\Debug\uDWM.dll
+        );
         if (!hFile)
         {
             printf("Unable to open system file.\n");
@@ -266,25 +210,42 @@ int main(int argc, char** argv)
         UnmapViewOfFile(lpFileBase);
         CloseHandle(hFileMapping);
         CloseHandle(hFile);  // Finished patching the file: C:\User\\Windows To Go\Desktop\Win11DisableRoundedCorners\x64\Debug\uDWM.dll
-        if (!CopyFileA(szDWM, szOriginalDWM, FALSE))  // Backup "C:\\Windows\\system32\\uDWM.dll" as "C:\\Windows\\system32\\uDWM_win11drc.bak"
+
+		/*
+		** ----- Finish Patching DLL using PDB File -----
+		*/
+
+        if (!CopyFileA((LPCSTR)szDWM.c_str(), (LPCSTR)szOriginalDWM.c_str(), FALSE))  // Rename: System32\\uDWM.dll => System32\uDWM_win11drc.bak
         {
             printf("Unable to backup system file.\n");
             _getch();
             return 9;
         }
-        if (!VnTakeOwnership(szDWM))  // TAKE OWNERSHIP: "C:\\Windows\\system32\\uDWM.dll"
+
+		char xxx[MAX_PATH];
+		ZeroMemory(
+			xxx,
+			(MAX_PATH) * sizeof(char)
+		);
+		strcpy(xxx, szDWM.c_str());
+
+		int num = MultiByteToWideChar(0, 0, xxx, -1, NULL, 0);
+		wchar_t* wide = new wchar_t[num];
+		MultiByteToWideChar(0, 0, xxx, -1, wide, num);
+
+        if (!VnTakeOwnership(wide))  // TAKE OWNERSHIP: System32\uDWM.dll
         {
             printf("Unable to take ownership of system file.\n");
             _getch();
             return 8;
         }
         strcat_s(
-            szOriginalDWM,
+			(char*)szOriginalDWM.c_str(),
             MAX_PATH,
             "1"
         );  // szOriginalDWM = "C:\\Windows\\system32\\uDWM_win11drc.bak1"
-        DeleteFileA(szOriginalDWM);  // Delete if existed: "C:\\Windows\\system32\\uDWM_win11drc.bak1"
-        if (!MoveFileA(szDWM, szOriginalDWM))  // Backup "C:\\Windows\\system32\\uDWM.dll" as "C:\\Windows\\system32\\uDWM_win11drc.bak1"
+        DeleteFileA((LPCSTR)szOriginalDWM.c_str());  // Delete if existed: "C:\\Windows\\system32\\uDWM_win11drc.bak1"
+        if (!MoveFileA(szDWM, (LPCSTR)szOriginalDWM.c_str()))  // Backup "C:\\Windows\\system32\\uDWM.dll" as "C:\\Windows\\system32\\uDWM_win11drc.bak1"
         {
             printf("Unable to prepare for replacing system file.\n");
             _getch();
@@ -297,37 +258,10 @@ int main(int argc, char** argv)
             return 10;
         }
         DeleteFileA(szModifiedDWM);  // Delete: "C:\\Users\\Windows To Go\\Desktop\\Win11DisableRoundedCorners\\x64\\Debug\\uDWM.dll"
+
+		KillDWM();
+		DeleteFileA((LPCSTR)szOriginalDWM.c_str());  // delete "C:\\Windows\\system32\\uDWM_win11drc.bak1"
     }
 
-	/*
-	** Kill dwm.exe
-	*/
-    STARTUPINFO si = { sizeof(si) };
-    PROCESS_INFORMATION pi;
-    BOOL b = CreateProcessA(
-        NULL,
-        szTaskkill,
-        NULL,
-        NULL,
-        TRUE,
-        CREATE_UNICODE_ENVIRONMENT,
-        NULL,
-        NULL,
-        &si,
-        &pi
-    );
-    WaitForSingleObject(pi.hProcess, INFINITE);
-    Sleep(10000);
-
-
-    if (bRestore)
-    {
-        DeleteFileA(szModifiedDWM);  // delete "C:\\Windows\\system32\\uDWMm.dll"
-    }
-    else
-    {
-        DeleteFileA(szOriginalDWM);  // delete "C:\\Windows\\system32\\uDWM_win11drc.bak1"
-    }
-    printf("Operation successful.\n");
-	return 0;
+    std::cout << "Operation successful." << std::endl;
 }
